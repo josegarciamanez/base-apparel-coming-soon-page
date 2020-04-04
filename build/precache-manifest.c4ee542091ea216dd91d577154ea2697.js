37f11beef44035b9c49e94a62fa1b6dd6123f4b6
José García Máñez <josegarciamanez@gmail.com>
2020-04-04 18:23:01 +0200

    add build

---
+++ build/precache-manifest.c4ee542091ea216dd91d577154ea2697.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f763ae6e6302155902755b3c9d4b4c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "485be1d39501e394cf3e",
+    "url": "/static/css/main.ec26e9dc.chunk.css"
+  },
+  {
+    "revision": "b885cad62fcb5baccd95",
+    "url": "/static/js/2.fbc54f02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fbc54f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485be1d39501e394cf3e",
+    "url": "/static/js/main.fb916241.chunk.js"
+  },
+  {
+    "revision": "8ec1a35c40081021c7ed",
+    "url": "/static/js/runtime-main.4e22f7a7.js"
+  },
+  {
+    "revision": "9a99295173be6c0f6dbc1e173d75ce48",
+    "url": "/static/media/bg-pattern-desktop.9a992951.svg"
+  },
+  {
+    "revision": "003b0c9744b45dea37e19234916edb43",
+    "url": "/static/media/hero-desktop.003b0c97.jpg"
+  },
+  {
+    "revision": "1d55799afb7254685302a38efea12fd1",
+    "url": "/static/media/hero-mobile.1d55799a.jpg"
+  },
+  {
+    "revision": "337809979308399afb36fa5f0e3fbcdf",
+    "url": "/static/media/icon-error.33780997.svg"
+  },
+  {
+    "revision": "877543ce69f4ed6f834f26e7dc4ca930",
+    "url": "/static/media/logo.877543ce.svg"
+  }
+]);
